a516cb3c54c4cb0cbe709f6cc8f341d5ad521dfc
Veikko Rytivaara <veikko.rytivaara@bittium.com>
2019-02-18 19:07:49 +0200

    Fix TODOs about reading configuration from application.properties
    Change-Id: I92ba36a0f4a107cc3f75995d2f98ade6e96d951a

--- src/main/java/com/bittium/qrapids/issuetracker/rest/IssueEndpoints.java
+++ src/main/java/com/bittium/qrapids/issuetracker/rest/IssueEndpoints.java
@@ -4,8 +4,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletResponse;
 import com.bittium.qrapids.issuetracker.interfaces.Jira;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +23,27 @@ import org.springframework.web.client.HttpStatusCodeException;
 
 @RestController
 public class IssueEndpoints {
+
+    @PostConstruct
+    public void init() {
+        // TODO: Make better mechanism to inform end user about empty properties
+        if (this.jiraServer.isEmpty()) {
+            System.out.println("jira.server is EMPTY");
+            SpringApplication.exit(this.appContext);
+        }
+        if (this.jiraUser.isEmpty()) {
+            System.out.println("jira.user is EMPTY");
+            SpringApplication.exit(this.appContext);
+        }
+        if (this.jiraPassword.isEmpty()) {
+            System.out.println("jira.password is EMPTY");
+            SpringApplication.exit(this.appContext);
+        }
+        if (this.jiraProjectKey.isEmpty()) {
+            System.out.println("jira.project.key is EMPTY");
+            SpringApplication.exit(this.appContext);
+        }
+    }
 
     /** this catches both client side and server side exceptions */
     @ExceptionHandler(value = HttpStatusCodeException.class)
@@ -48,15 +74,29 @@ public class IssueEndpoints {
             }
         }
 
-        Jira jira = new Jira();
-        // TODO: RytiVei: Read these from config. Now replace by hand and rebuild for testing.
-        jira.createClient("replace_me_SERVER", "replace_me_USER", "replace_me_PASSWORD");
-
-        // TODO: RytiVei: Read projectkey from config. Now replace by hand and rebuild for testing.
+        jira.createClient(this.jiraServer, this.jiraUser, this.jiraPassword);
         CreateIssueResponse response =
-                jira.createIssue("replace_me_PROJECTKEY", issue.get("issue_type"),
+                jira.createIssue(this.jiraProjectKey, issue.get("issue_type"),
                         issue.get("issue_summary"), issue.get("issue_description"));
 
         return response;
     }
+
+    @Value("${jira.server}")
+    private String jiraServer;
+
+    @Value("${jira.user}")
+    private String jiraUser;
+
+    @Value("${jira.password}")
+    private String jiraPassword;
+
+    @Value("${jira.project.key}")
+    private String jiraProjectKey;
+
+    @Autowired
+    private ApplicationContext appContext;
+
+    @Autowired
+    private Jira jira;
 }
